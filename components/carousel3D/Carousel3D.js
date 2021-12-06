8ba6e5b07cca74c961c782ebd3988022bd76df10
Kris <heijnenkris@gmail.com>
2021-12-06 17:16:29 +0100

    commit

--- components/carousel3D/Carousel3D.js
+++ components/carousel3D/Carousel3D.js
@@ -12,6 +12,7 @@ import { GoPrimitiveDot } from "react-icons/go";
 import { BiRightArrowCircle } from "react-icons/bi";
 import { VscTrash } from "react-icons/vsc";
 import WriteAnimation from "../writeAnimation/WriteAnimation";
+import ObserverComponent from "../observerComponent/ObserverComponent";
 
 const Carousel3D = () => {
   const [currentDeg, setCurrentDeg] = useState(0);
@@ -58,104 +59,114 @@ const Carousel3D = () => {
   }, [projectsState]);
 
   return (
-    <div className="carousel3D">
-      <div className="carousel3D__container">
-        <div
-          className="carousel3D__wrapper"
-          style={{
-            transform: `rotateY(${currentDeg}deg)`,
-          }}
-        >
-          {projectsState.map((project, index) => {
-            project.description = parseToHTML(project.description);
-            return (
-              <div
-                className={`project ${
-                  index == currentIndex && "project--active"
-                } ${index == projectToBeDeleted ? "disappear" : ""}`}
-                style={{
-                  transform: `rotateY(${turnDeg * index}deg) translateZ(400px)`,
-                }}
-                onClick={index != currentIndex ? () => clicked(index) : null}
-                key={project.title}
-              >
-                <h4 className="project__title">{project.title}</h4>
-                <div className="project__image">
-                  <img
-                    className="project__image"
-                    src={`/static/images/${project.image}`}
-                    alt={project.title}
-                  />
-                  {index == currentIndex && (
-                    <video
-                      autoPlay={true}
-                      loop
-                      src={`/static/videos/${project.video}`}
-                    ></video>
-                  )}
-                </div>
-
-                <p
-                  className="project__description"
-                  dangerouslySetInnerHTML={{ __html: project.description }}
-                ></p>
-                <a
-                  className="project__link"
-                  href={project.url}
-                  target="_blank"
-                  rel="noreferrer"
+    <ObserverComponent
+      addClass={["projects-in-view"]}
+      options={{ threshold: "0.5" }}
+    >
+      <div className="carousel3D">
+        <div className="carousel3D__container">
+          <div
+            className="carousel3D__wrapper"
+            style={{
+              transform: `rotateY(${currentDeg}deg)`,
+            }}
+          >
+            {projectsState.map((project, index) => {
+              project.description = parseToHTML(project.description);
+              return (
+                <div
+                  className={`project ${
+                    index == currentIndex ? "project--active" : ""
+                  } ${index == projectToBeDeleted ? "disappear" : ""}`}
+                  style={{
+                    transform: `rotateY(${
+                      turnDeg * index
+                    }deg) translateZ(400px)`,
+                  }}
+                  onClick={index != currentIndex ? () => clicked(index) : null}
+                  key={project.title}
                 >
-                  {project.url}
-                </a>
-              </div>
-            );
-          })}
+                  <h4 className="project__title">{project.title}</h4>
+                  <div className="project__image">
+                    <img
+                      className="project__image"
+                      src={`/static/images/${project.image}`}
+                      alt={project.title}
+                    />
+                    {index == currentIndex && (
+                      <video
+                        autoPlay={true}
+                        loop
+                        src={`/static/videos/${project.video}`}
+                      ></video>
+                    )}
+                  </div>
+                  <p
+                    className="project__description"
+                    dangerouslySetInnerHTML={{ __html: project.description }}
+                  ></p>
+                  <a
+                    className="project__link"
+                    href={project.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {project.url}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
+        {projectsState.length > 0 && (
+          <div className="controls">
+            <BiRightArrowCircle
+              color="white"
+              style={{ transform: "rotate(180deg)" }}
+              size="3em"
+              className="arrow"
+              onClick={
+                projectsState[currentIndex - 1]
+                  ? () => clicked(currentIndex - 1)
+                  : () => clicked(projectsState.length - 1)
+              }
+            />
+            <div>
+              {projectsState.map((project, index) => {
+                return (
+                  <GoPrimitiveDot
+                    key={project.title}
+                    className={
+                      index == currentIndex ? "dot dot--active" : "dot"
+                    }
+                  />
+                );
+              })}
+            </div>
+            <BiRightArrowCircle
+              size="3em"
+              className="arrow"
+              onClick={
+                projectsState[currentIndex + 1]
+                  ? () => clicked(currentIndex + 1)
+                  : () => clicked(0)
+              }
+            />
+            <VscTrash
+              className="delete"
+              size="2em"
+              onClick={() => remove(currentIndex)}
+            />
+          </div>
+        )}
+        {projectsState.length == 0 && (
+          <div className="reset">
+            <WriteAnimation sentence="Great, now I have no projects left. Please reset!" />
+            <button onClick={() => setProjectsState(projects)}>Reset</button>
+          </div>
+        )}
       </div>
-      <div className="controls">
-        <BiRightArrowCircle
-          color="white"
-          style={{ transform: "rotate(180deg)" }}
-          size="3em"
-          className="arrow"
-          onClick={
-            projectsState[currentIndex - 1]
-              ? () => clicked(currentIndex - 1)
-              : () => clicked(projectsState.length - 1)
-          }
-        />
-        <div>
-          {projectsState.map((project, index) => {
-            return (
-              <GoPrimitiveDot
-                key={project.title}
-                className={index == currentIndex ? "dot dot--active" : "dot"}
-              />
-            );
-          })}
-        </div>
-        <BiRightArrowCircle
-          size="3em"
-          className="arrow"
-          onClick={
-            projectsState[currentIndex + 1]
-              ? () => clicked(currentIndex + 1)
-              : () => clicked(0)
-          }
-        />
-        <VscTrash
-          className="delete"
-          size="2em"
-          onClick={() => remove(currentIndex)}
-        />
-      </div>
-      {projectsState.length == 0 && (
-        <div className="reset">
-          <WriteAnimation sentence="Great, now I have no projects left. Please reset!" />
-          <button onClick={() => setProjectsState(projects)}>Reset</button>
-        </div>
-      )}
-    </div>
+    </ObserverComponent>
   );
 };
 
--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,3 +1,5 @@
+import { BsGithub } from "react-icons/bs";
+
 const Navbar = ({ visibility, scrollToSection }) => {
   return (
     <nav
@@ -8,20 +10,32 @@ const Navbar = ({ visibility, scrollToSection }) => {
           onClick={() => scrollToSection("projects")}
           className="navbar__link projects-link"
         >
-          Projects
+          <span className="letter">P</span>
+          <span className="word">Projects</span>
         </button>
         <button
           onClick={() => scrollToSection("courses")}
           className="navbar__link courses-link"
         >
-          Courses
+          <span className="letter">C</span>
+          <span className="word">Courses</span>
         </button>
         <button
           onClick={() => scrollToSection("about")}
           className="navbar__link about-link"
         >
-          About
+          <span className="letter">A</span>
+          <span className="word">About</span>
         </button>
+      </div>
+      <div className="navbar__github">
+        <a
+          href="https://github.com/droomoord/kris-heijnen"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub size="2.5em" />
+        </a>
       </div>
     </nav>
   );
