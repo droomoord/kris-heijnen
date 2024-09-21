a8b3d8a9edd08fb3064c6cd41afaee989d8f69f3
Droomoord <54590481+droomoord@users.noreply.github.com>
2024-09-21 14:05:28 +0200

    fix write sentence bug

--- components/writeAnimation/WriteAnimation.js
+++ components/writeAnimation/WriteAnimation.js
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 
-const WriteAnimation = ({ sentence, delay }) => {
+const WriteAnimation = ({ sentence, delay = 0 }) => {
   const [result, setResult] = useState("");
+
   useEffect(() => {
-    setTimeout(() => {
-      let index = 0;
-      const interval = setInterval(addLetter, 200);
-      function addLetter() {
-        if (sentence.length === index) clearInterval(interval);
-        setResult((prev) => prev + sentence.charAt(index));
+    let index = 0;
+
+    const typeLetter = () => {
+      if (index < sentence.length) {
+        setResult(sentence.slice(0, index + 1)); // Slice the sentence up to the current index
         index++;
+      } else {
+        clearInterval(interval);
       }
-    }, delay || 0);
+    };
+
+    const interval = setInterval(typeLetter, 200); // Adjust typing speed
+    setTimeout(() => {
+      typeLetter(); // Start typing immediately after delay
+    }, delay);
+
+    return () => clearInterval(interval); // Clean up on unmount
+
   }, [sentence, delay]);
+
   return <span className="writeAnimation">{result}</span>;
 };
+
 export default WriteAnimation;
